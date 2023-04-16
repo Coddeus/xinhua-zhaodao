3363bc7f1196d6bd0b02768db81833a8afbecaaa
Coddeus <codeusbonus@gmail.com>
2023-04-16 16:39:24 +0200

    New query: looks for a radical in other characters According to console user input Using SQLite Ready for realease 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 use sqlite;
+use std::io::stdin;
 
-fn main(){
+fn main() -> Result<(), ()>{
 	let conn = sqlite::open("./src/data/hanyu.db").unwrap();
 
-	let query = "select * from cidian limit 10";
+    let mut component = String::new();
+    stdin().read_line(&mut component).unwrap();
+    let component = component.trim();
+
+	let query = format!("{}{}{}{}{}{}{}",
+	"select word from 
+	(
+			select word from zidian 
+		INTERSECT 
+			select ch as word from decomp 
+				where (ch like '%", component, "%' OR p1 like '%", component, "%' OR p2 like '%", component, "%')
+	) result 
+	
+	join decomp on result.word = decomp.ch 
+
+	order by s asc");
 	let mut statement = conn.prepare(query).unwrap();
 
 	while let Ok(sqlite::State::Row) = statement.next() {
-	    println!("{}: ", statement.read::<String, _>("ci").unwrap());
-	    println!("{}\n", statement.read::<String, _>("explanation").unwrap());
+	    println!("- {}", statement.read::<String, _>("word").unwrap());
 	}
+
+	Ok(())
 }
