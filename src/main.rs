ac8bf5d6be8049b65c79bcc0d6ac7c4493a3bfbb
Coddeus <codeusbonus@gmail.com>
2023-04-16 12:44:05 +0200

    Added an SQLite DB for Chinese Linked it to main.rs (with a example request) Added cargo check to Github Actions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use sqlite;
+
+fn main(){
+	let conn = sqlite::open("./src/data/hanyu.db").unwrap();
+
+	let query = "select * from cidian limit 10";
+	let mut statement = conn.prepare(query).unwrap();
+
+	while let Ok(sqlite::State::Row) = statement.next() {
+	    println!("{}: ", statement.read::<String, _>("ci").unwrap());
+	    println!("{}\n", statement.read::<String, _>("explanation").unwrap());
+	}
 }
