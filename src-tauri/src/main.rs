666ef2f0684d2c389c76a03b30b7787698648297
Coddeus <codeusbonus@gmail.com>
2023-04-21 18:20:42 +0200

    🛠 Adapted GUI to DB search - Input: Search DB form - Output: Selectable text, with all chars concatenated

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,43 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use sqlite::*;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn derivatives(cmp: &str) -> String{
+	let conn = sqlite::open("./data/hanyu.db").unwrap();
+    let component = cmp.trim();
+    let mut derivatives: String = "".to_owned();
+
+	let query = format!("{}{}{}{}{}{}{}",
+	"select word from 
+	(
+			select word from zidian 
+		INTERSECT 
+			select ch as word from decomp 
+				where (ch like '%", component, "%' OR p1 like '%", component, "%' OR p2 like '%", component, "%')
+	) result 
+	
+	join decomp on result.word = decomp.ch 
+
+	order by s asc");
+	let mut statement = conn.prepare(query).unwrap();
+
+	while let Ok(sqlite::State::Row) = statement.next() {
+	    derivatives = format!("{}{}", derivatives, statement.read::<String, _>("word").unwrap());
+	}
+
+    derivatives
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, derivatives])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
